427340147171a2a0b2635f7a8f27d640e9aa3a19
sanposhiho <handbomusic@gmail.com>
2020-09-04 14:39:09 +0900

    fix: testcase

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -5,8 +5,8 @@ func p(x int) int {
 }
 
 func f(param int) int {
-	useOutOfIf := 1212121
-	ret := 0 // want "wasted assignment"
+	useOutOfIf := 1212121 // want "wasted assignment"
+	ret := 0              // want "wasted assignment"
 	if false {
 		useOutOfIf = 10 // want "wasted assignment"
 		useOutOfIf = 10 // want "reassigned, but never used afterwards"
